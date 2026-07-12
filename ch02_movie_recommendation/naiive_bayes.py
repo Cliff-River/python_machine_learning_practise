944bf4755cfa9d7cac96004c48a8257e3ac601b7
Cliff Huang <hzf510920@163.com>
2026-07-13 00:06:20 +0800

    Add ch02 movie recommendation module and update dependencies

---
+++ ch02_movie_recommendation/naiive_bayes.py
@@ -0,0 +1,27 @@
+# %% packages
+import numpy as np
+from collections import defaultdict
+
+# %% define dataset
+X_train = np.array(
+    [
+        [0, 1, 1],
+        [0, 0, 1],
+        [0, 0, 0],
+        [1, 1, 0]
+    ]
+)
+Y_train = ["Y", "N", "Y", "Y"]
+X_test = np.array([1, 1, 0])
+
+# %% define function to get label indices
+def get_label_indices(labels):
+    label_indices = defaultdict(list)
+    for index, label in enumerate(labels):
+        label_indices[label].append(index)
+    return label_indices
+    
+label_indices = get_label_indices(Y_train)
+print(label_indices)
+
+# %%
